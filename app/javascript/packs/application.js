02472effe82cabad2008aa147c98c0f98fd55aa8
adeelahmadrasheed <adeelahmadrasheed@gmail.com>
2022-04-28 03:24:22 +0500

    added and implementedjQuery and bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,24 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+//= require bootstrap-sprockets
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap'
+
+require("bootstrap/dist/js/bootstrap")
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("packs/custom")
+require("packs/posts.js")
+
+
 
 Rails.start()
 Turbolinks.start()
