0c2fdef123fb75b56780aa9697306c9c32741b11
adeelahmadrasheed <adeelahmadrasheed@gmail.com>
2022-05-14 18:30:17 +0500

    Style Partials Form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,6 @@ class ArticlesController < ApplicationController
 	
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
 		flash[:notice] = "Article deleted successfully."
 		redirect_to articles_path
